74d7f27dbf3923bcd2df8a4f9cc2dbcaa6b78eba
Anjul Sahu <anjul@infracloud.io>
2021-02-02 17:54:26 +0530

    removed hardcode variable

--- rman.sh
+++ rman.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/bash
 
 echo "Inside RMAN.sh"
-echo "connect_string=$1"
+# echo "connect_string=$1"
+connect_string="sys/Kube#2020@//rman-research:1521/ORCLCDB"
+connect_string=$1
 command=$2
 return_code=0
 if [ $command = "list" ]; then
-rman target sys/Kube#2020@//rman-research:1521/ORCLCDB cmdfile=list.rman
+rman target $connect_string cmdfile=list.rman
 return_code=$?
 elif [ $command = "backupcontrolfile" ]; then
-rman target sys/Kube#2020@//rman-research:1521/ORCLCDB cmdfile=backupcontrolfile.rman
+rman target $connect_string cmdfile=backupcontrolfile.rman
 return_code=$?
 else
 echo "command is not yet implemented"
